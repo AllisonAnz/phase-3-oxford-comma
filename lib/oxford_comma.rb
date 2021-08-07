d390bb578e403121f55eab9de9ff0da5a1b98d3b
AllisonAnz <AllisonAnzalone@gmail.com>
2021-08-07 15:53:06 -0500

    Completed Assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
+#oxford_comma
+  #returns a string without any additional formatting when given a 1-element array 
+  #adds 'and' between elements when given a 2-element array 
+  #adds commas plus a final 'and' when given a 3-element array 
+  #correctly formats arrays of lengths greater than three
+
+array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"]
 def oxford_comma(array)
+    return array.join(" and ") if array.size < 3 
+
+    array[-1] = "and #{array[-1]}"
+
+    array.join(", ")
 
 end
+
+puts oxford_comma(array)
+#=> kiwi, durian, starfruit, mangos, dragon fruits, lychees, and pomelos
